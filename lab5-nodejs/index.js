072f1db1461b8a5beb69740fee20e22baf937a1b
Jade Apers <jadeapers@telenet.be>
2021-04-03 20:37:47 +0200

    added GET route messages

--- lab5-nodejs/index.js
+++ lab5-nodejs/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/v1/messages', function(req, res){
+    res.send("getting messages");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
